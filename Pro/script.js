9d45447967d78e699a3c44228c1f25cd3d499556
VGameleira.Dev <vinigameleira.stos@gmail.com>
2025-03-17 21:53:06 -0300

    'main'

--- Pro/script.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.getElementById("darkModeToggle")
-
-const body = document.body
-
-toggleButton.addEventListener("click", () => { body.classeList.toggle("dark-Mode") });
-
-if (body.classList.contains("dark-mode")) {
-    localStorage.setItem("darkMode", "enabled");
-} else { localStorage.setItem("darkMode", "disabled"); }
